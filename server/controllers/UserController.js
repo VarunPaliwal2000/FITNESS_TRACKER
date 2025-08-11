444f27efe7b5c1ce9152e8f494c5faccbeae535f
Varun <varunr.paliwal@gmail.com>
2025-08-11 14:26:44 +0530

    models setup fo ruser and workout

---
+++ server/controllers/UserController.js
@@ -0,0 +1,8 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { createError } from "../error.js";
+import User from "../models/User.js";
+import Workout from "../models/Workout.js";
+
+dotenv.config();
